7d40b818e42f7d08727f9da1dbc121eac5415d8c
ihza.poli <ihza.poli@mandalafinance.com>
2023-12-06 16:13:51 +0700

    style(detail-page): enhance layout styling and add back button

--- src/components/SimpleCard/index.tsx
+++ src/components/SimpleCard/index.tsx
@@ -7,7 +7,7 @@ const SimpleCard = (props: SimpleCardProps) => {
   const { label, description } = props;
 
   return (
-    <div className="rounded-lg p-4 bg-slate-800 border-2 border-slate-300 text-white">
+    <div className="rounded-lg p-4 text-white">
       <div className="font-medium text-base mb-4 border-b-2 border-white">{label}</div>
       <div className="text-xl">{description}</div>
     </div>
--- src/pages/detail/[itemId].tsx
+++ src/pages/detail/[itemId].tsx
@@ -4,6 +4,7 @@ import { MainStyled, Planet } from '../index';
 import styles from '@styles/Home.module.css';
 import { ParsedUrlQuery } from 'querystring';
 import SimpleCard from 'components/SimpleCard';
+import { ChevronLeft } from 'react-feather';
 
 interface DetailPageProps {
   data: Planet;
@@ -11,11 +12,19 @@ interface DetailPageProps {
 
 const Detail: NextPage<DetailPageProps> = (props) => {
   const { data } = props;
+  const formattedPopulation = data.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  const router = useRouter();
 
   return (
     <div className={styles.container}>
+      <button
+        onClick={() => router.back()}
+        className="rounded-full transition bg-slate-800 hover:bg-slate-400 p- w-16 h-16 flex justify-center items-center absolute left-10 top-12 text-white"
+      >
+        <ChevronLeft />
+      </button>
       <MainStyled>
-        <h1 className="text-white font-semibold text-4xl mb-8">{data.name}</h1>
+        <h1 className="text-white font-semibold text-6xl mb-8">{data.name}</h1>
         <div className="grid grid-cols-12 gap-4 w-full">
           <div className="col-start-3 col-span-2">
             <SimpleCard label="Climate" description={data.climate} />
@@ -25,6 +34,28 @@ const Detail: NextPage<DetailPageProps> = (props) => {
           </div>
           <div className="col-start-9 col-span-2">
             <SimpleCard label="Diameter" description={`${data.diameter} Km`} />
+          </div>
+          <div className="col-span-full">
+            <div className="bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100 grid grid-cols-12">
+              <div className="col-start-2 col-span-2">
+                <SimpleCard label="Population" description={`${formattedPopulation} people`} />
+              </div>
+              <div className="col-span-2">
+                <SimpleCard label="Orbital Period" description={`${data.orbital_period} days`} />
+              </div>
+              <div className="col-span-2">
+                <SimpleCard
+                  label="Rotational Period"
+                  description={`${data.rotation_period} days`}
+                />
+              </div>
+              <div className="col-span-2">
+                <SimpleCard label="Gravity" description={`${data.gravity}`} />
+              </div>
+              <div className="col-span-2">
+                <SimpleCard label="Surface Water" description={`${data.surface_water}%`} />
+              </div>
+            </div>
           </div>
         </div>
       </MainStyled>
